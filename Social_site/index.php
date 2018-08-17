ed011bfea2b3a3ad7dc78a3a5afc8df0092669a6
Joe Birech <jobirech@e1z4r13p9.42.us.org>
2018-08-17 00:11:22 -0700

    added php and mysql for managing the database and the login informations for the user

---
+++ Social_site/index.php
@@ -0,0 +1,32 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    index.php                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jobirech <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2018/08/17 00:09:33 by jobirech          #+#    #+#              #
+#    Updated: 2018/08/17 00:09:42 by jobirech         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+<?php  
+$con = mysqli_connect("localhost", "root", "", "social"); //Connection variable
+
+if(mysqli_connect_errno()) 
+{
+	echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+$query = mysqli_query($con, "INSERT INTO test VALUES ('2', 'Optimus Prime')");
+
+?>
+
+<html>
+<head>
+	<title>Welcome to Swirlfeed</title>
+</head>
+<body>
+	Hello Reece!!!!!
+</body>
+</html>
