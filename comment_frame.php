55d4d7c91c0721721e6aaf2d76e33120cd525547
Joe Birech <jobirech@e1z3r1p5.42.us.org>
2018-08-24 00:09:07 -0700

    added the iframe for replying to comments

--- comment_frame.php
+++ comment_frame.php
@@ -51,7 +51,7 @@
             $post_body = $_POST['post_body'];
             $post_body = mysqli_escape_string($con, $post_body);
             $date_time_now = date("Y-m-d H:i:s");
-            $insert_post = mysqli_query($con, "INSERT INTO comments VALUES ('', '$post_body', '$userLoggedIn', '$posted_to', '$date_time_now', 'no', '$post_id'");
+            $insert_post = mysqli_query($con, "INSERT INTO comments VALUES ('', '$post_body', '$userLoggedIn', '$posted_to', '$date_time_now', 'no', '$post_id')");
             echo "<p>Comment Added! </p>";
         }
 
@@ -62,7 +62,94 @@
             <input type="submit" name="postComment<?php echo $post_id; ?>" value="post">
         </form>
 
+        <!-- load comments -->
+        <?php
+        $get_comments = mysqli_query($con, "SELECT * FROM comments WHERE post_id='$post_id' ORDER BY id ASC");
+        $count = mysqli_num_rows($get_comments);
         
+        if ($count != 0)
+        {
+            while($comment = mysqli_fetch_array($get_comments))
+            {
+                $comment_body = $comment['post_body'];
+                $posted_to = $comment['posted_to'];
+                $posted_by = $comment['posted_by'];
+                $date_added = $comment['date_added'];
+                $removed = $comment['removed']; 
+                
+
+                //Timeframe
+                $date_time_now = date("Y-m-d H:i:s");
+                $start_date = new DateTime($date_time); //Time of post
+                $end_date = new DateTime($date_time_now); //Current time
+                $interval = $start_date->diff($end_date); //Difference between dates 
+                if($interval->y >= 1) {
+                    if($interval == 1)
+                        $time_message = $interval->y . " year ago"; //1 year ago
+                    else 
+                        $time_message = $interval->y . " years ago"; //1+ year ago
+                }
+                else if ($interval-> m >= 1) {
+                    if($interval->d == 0) {
+                        $days = " ago";
+                    }
+                    else if($interval->d == 1) {
+                        $days = $interval->d . " day ago";
+                    }
+                    else {
+                        $days = $interval->d . " days ago";
+                    }
+
+
+                    if($interval->m == 1) {
+                        $time_message = $interval->m . " month". $days;
+                    }
+                    else {
+                        $time_message = $interval->m . " months". $days;
+                    }
+
+                }
+                else if($interval->d >= 1) {
+                    if($interval->d == 1) {
+                        $time_message = "Yesterday";
+                    }
+                    else {
+                        $time_message = $interval->d . " days ago";
+                    }
+                }
+                else if($interval->h >= 1) {
+                    if($interval->h == 1) {
+                        $time_message = $interval->h . " hour ago";
+                    }
+                    else {
+                        $time_message = $interval->h . " hours ago";
+                    }
+                }
+                else if($interval->i >= 1) {
+                    if($interval->i == 1) {
+                        $time_message = $interval->i . " minute ago";
+                    }
+                    else {
+                        $time_message = $interval->i . " minutes ago";
+                    }
+                }
+                else {
+                    if($interval->s < 30) {
+                        $time_message = "Just now";
+                    }
+                    else {
+                        $time_message = $interval->s . " seconds ago";
+                    }
+                }    
+
+                $user_obj = new User($con, $posted_by);
+
+            }
+        }
+        ?>
+        <div class="comment_section">
+            <a href="<?php echo $posted_by?>">prince</a>
+        </div>
     
 </body>
 </html>
