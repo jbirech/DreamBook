0f7c911095cbaae6a8c7f119b27bd38284ffb593
Joe Birech <jobirech@e1z4r13p11.42.us.org>
2018-09-05 13:19:10 -0700

    yeah didn't do much help either

--- includes/header.php
+++ includes/header.php
@@ -1,139 +1,144 @@
 <?php  
 require 'config/config.php';
-include("includes/classes/User.php");
-include("includes/classes/Post.php");
-include("includes/classes/Message.php");
+// include("includes/classes/User.php");
+// include("includes/classes/post.php");
+// include("includes/classes/Message.php");
+//include("includes/classes/Notifications.php");
 
 
-if (isset($_SESSION['username'])) {
-	$userLoggedIn = $_SESSION['username'];
-	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
-	$user = mysqli_fetch_array($user_details_query);
+//require_once("all_includes.php");
+
+if (isset($_SESSION['username']))
+{
+    $userLoggedIn= $_SESSION['username'];
+    $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+    $user = mysqli_fetch_array($user_details_query);
 }
-else {
-	header("Location: register.php");
+else
+{
+    header("Location: register.php");
 }
-
 ?>
 
 <html>
 <head>
-	<title>Welcome to Swirlfeed</title>
+    <title>Dreambook</title>
 
-	<!-- Javascript -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="assets/js/bootstrap.js"></script>
-	<script src="assets/js/bootbox.min.js"></script>
-	<script src="assets/js/Dreambook.js"></script>
-	<script src="assets/js/jquery.jcrop.js"></script>
-	<script src="assets/js/jcrop_bits.js"></script>
+    <!-- Javascript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/bootbox.min.js"></script>
+    <script src="assets/js/Dreambook.js"></script>
+    <script src="assets/js/jquery.jcrop.js"></script>
+    <script src="assets/js/jcrop_bits.js"></script>
+   
+    <!-- Css -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/jquery.Jcrop.css">
 
-
-	<!-- CSS -->
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-	<link rel="stylesheet" href="assets/css/jquery.Jcrop.css" type="text/css" />
 </head>
 <body>
 
-	<div class="top_bar"> 
+    <div class="top_bar">
+        <div class="logo">
+            <a href="index.php">Dreambook</a>
+        </div>
 
-		<div class="logo">
-			<a href="index.php">Dreambook</a>
-		</div>
-
-		<nav>
-			<?php
-				//Unread messages 
-				$messages = new Message($con, $userLoggedIn);
-				$num_messages = $messages->getUnreadNumber();
-			?>
+        <nav>
+            <?php
+               // $messages = new Message($con, $userLoggedIn);
+               // $num_messages = $messages->getUnreadNumber();
+            ?>
 
 
-			<a href="<?php echo $userLoggedIn; ?>">
-				<?php echo $user['first_name']; ?>
-			</a>
-			<a href="index.php">
-				<i class="fa fa-home fa-lg"></i>
-			</a>
-			<a href="javascript:void(0);" onclick="getDropdownData('<?php echo $userLoggedIn; ?>', 'message')">
-				<i class="fa fa-envelope fa-lg"></i>
-				<?php
-				if($num_messages > 0)
-				 echo '<span class="notification_badge" id="unread_message">' . $num_messages . '</span>';
-				?>
-			</a>
-			<a href="#">
-				<i class="fa fa-bell fa-lg"></i>
-			</a>
-			<a href="requests.php">
-				<i class="fa fa-users fa-lg"></i>
-			</a>
-			<a href="#">
-				<i class="fa fa-cog fa-lg"></i>
-			</a>
-			<a href="includes/handlers/logout.php">
-				<i class="fa fa-sign-out fa-lg"></i>
-			</a>
+            <a href="<?php echo $userLoggedIn; ?>">
+                <?php echo $user['first_name'];?>
+            </a>
 
-		</nav>
+            <a href="index.php">
+                <i class="fa fa-home fa-lg"></i>
+            </a>
+            
+            <a href="javascript:void(0);" onclick="getDropdownData('<?php echo $userLoggedIn; ?>', 'message')">
+                <i class="fa fa-envelope fa-lg"></i>
+                <?php
+                //if ($num_messages > 0)
+                  //  echo ' <span class="notifications_badge" id="unread_message">'. $num_messages . '</span>'
+                ?>
+            </a>
+            
+            <a href="#">
+                <i class="fa fa-bell-o fa-lg">       
+                </i>
+            </a>
 
-		<div class="dropdown_data_window" style="height:0px; border:none;"></div>
-		<input type="hidden" id="dropdown_data_type" value="">
+            <a href="requests.php">
+                <i class="fa fa-users fa-lg">
+                </i>
+            </a>
 
+            <a href="#">
+                <i class="fa fa-cog fa-lg">
+                </i>
+            </a>
+            <a href="includes/handlers/logout.php">
+                <i class="fa fa-sign-out-alt fa-lg">       
+                </i>
+            </a>
+        </nav>
 
-	</div>
+        <div class="dropdown_data_window" style="height:0px; border:none;"></div>
+        <input type="hidden" id="dropdown_data_type" value="">
 
+    </div>
 
-	<script>
+    <script>
 	var userLoggedIn = '<?php echo $userLoggedIn; ?>';
 
-	$(document).ready(function() {
+	$(document).ready(function()
+	{
 
-		$('.dropdown_data_window').scroll(function() {
-			var inner_height = $('.dropdown_data_window').innerHeight(); //Div containing data
+		$('.dropdown_data_window').scroll(function()
+		{
+			var inner_height = $('.dropdown_data_window').innerHeight();//div containing photos
 			var scroll_top = $('.dropdown_data_window').scrollTop();
 			var page = $('.dropdown_data_window').find('.nextPageDropdownData').val();
 			var noMoreData = $('.dropdown_data_window').find('.noMoreDropdownData').val();
 
-			if ((scroll_top + inner_height >= $('.dropdown_data_window')[0].scrollHeight) && noMoreData == 'false') {
+			if ((scroll_top + inner_height >=('.dropdown_data_window')[0].scrollHeight) && noMoreData == 'false')
+			{
+                var pageName; //holds name of page to send ajax request to
+                var type = $('.dropdown_data_type').val();
 
-				var pageName; //Holds name of page to send ajax request to
-				var type = $('#dropdown_data_type').val();
+                if(type == 'notifications')
+                    pageName = "ajax_load_notifications.php";
+                else if(type == 'message')
+                    pageName = "ajax_load_messages.php";
 
-
-				if(type == 'notification')
-					pageName = "ajax_load_notifications.php";
-				else if(type = 'message')
-					pageName = "ajax_load_messages.php"
-
-
-				var ajaxReq = $.ajax({
+				var ajaxReq = $.ajax(
+				{
 					url: "includes/handlers/" + pageName,
 					type: "POST",
-					data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+					data: "page=" + page + "&userLoggedIn" + userLoggedIn,
 					cache:false,
 
-					success: function(response) {
-						$('.dropdown_data_window').find('.nextPageDropdownData').remove(); //Removes current .nextpage 
-						$('.dropdown_data_window').find('.noMoreDropdownData').remove(); //Removes current .nextpage 
-
+					success: function(response)
+					{
+						$('.dropdown_data_window').find('.nextPageDropdownData').remove(); //Removes current .nextpage
+						$('.dropdown_data_window').find('.noMoreDropdownData').remove(); //Removes current .nextpage
 
 						$('.dropdown_data_window').append(response);
 					}
 				});
-
-			} //End if 
+			}//End if
 
 			return false;
 
-		}); //End (window).scroll(function())
-
-
+		});// End (window).scroll(function()
 	});
 
-	</script>
-
-
-	<div class="wrapper">
+</script>
+ 
+    <div class="wrapper">
