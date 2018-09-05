79da9d2e073c5b548af323f76df874fc6357da09
Joe Birech <joebir98@gmail.com>
2018-09-05 14:38:11 -0700

    test cases for classes version 7*

---
+++ bookdream/test/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<?php
+$con = mysqli_connect("localhost", "root", "", "test");//connection variable
+
+if(mysqli_connect_errno())
+{
+   echo "failed to connect: ". mysqli_connect_errno();
+}
+
+?>
