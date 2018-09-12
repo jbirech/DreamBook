7d4258e09849c5b120dab1116961595642a18fc3
Joe Birech <jobirech@e1z4r13p11.42.us.org>
2018-09-12 16:45:51 -0700

    messages is not working properly

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -1,14 +1,17 @@
 <?php
-class Message {
+class Message 
+{
 	private $user_obj;
 	private $con;
 
-	public function __construct($con, $user){
+    public function __construct($con, $user)
+    {
 		$this->con = $con;
 		$this->user_obj = new \One\User($con, $user);
 	}
 
-	public function getMostRecentUser() {
+    public function getMostRecentUser() 
+    {
 		$userLoggedIn = $this->user_obj->getUsername();
 
 		$query = mysqli_query($this->con, "SELECT user_to, user_from FROM messages WHERE user_to='$userLoggedIn' OR user_from='$userLoggedIn' ORDER BY id DESC LIMIT 1");
@@ -27,7 +30,8 @@ class Message {
 
 	}
 
-	public function sendMessage($user_to, $body, $date) {
+    public function sendMessage($user_to, $body, $date) 
+    {
 
 		if($body != "") {
 			$userLoggedIn = $this->user_obj->getUsername();
@@ -35,7 +39,8 @@ class Message {
 		}
 	}
 
-	public function getMessages($otherUser) {
+    public function getMessages($otherUser) 
+    {
 		$userLoggedIn = $this->user_obj->getUsername();
 		$data = "";
 
@@ -43,20 +48,26 @@ class Message {
 
 		$get_messages_query = mysqli_query($this->con, "SELECT * FROM messages WHERE (user_to='$userLoggedIn' AND user_from='$otherUser') OR (user_from='$userLoggedIn' AND user_to='$otherUser')");
 
-		while($row = mysqli_fetch_array($get_messages_query)) {
+        while($row = mysqli_fetch_array($get_messages_query)) 
+        {
 			$user_to = $row['user_to'];
 			$user_from = $row['user_from'];
 			$body = $row['body'];
 			$id = $row['id'];
 
-			$div_top = ($user_to == $userLoggedIn) ? "<div class='message' id='green'>" : "<div class='message' id='blue'>";
-			$button = "<span class='deleteButton' onclick='deleteMessage($id, this)'>X</span>";
+            //Display a color either blue or green depending on if you are sending or receiving the message
+            $div_top = ($user_to == $userLoggedIn) ? "<div class='message' id='green'>" : "<div class='message' id='blue'>";
+            
+            //NOTE check on the delete button later on is't working on properly
+            //Error message deleteMessage not defined.
+			// $button = "<span class='deleteButton' onclick='deleteMessage($id, this)'>X</span>";
 			$data = $data . $div_top . $button . $body . "</div><br><br>";
 		}
 		return $data;
 	}
 
-	public function getLatestMessage($userLoggedIn, $user2) {
+    public function getLatestMessage($userLoggedIn, $user2) 
+    {
 		$details_array = array();
 
 		$query = mysqli_query($this->con, "SELECT body, user_to, date FROM messages WHERE (user_to='$userLoggedIn' AND user_from='$user2') OR (user_to='$user2' AND user_from='$userLoggedIn') ORDER BY id DESC LIMIT 1");
@@ -69,61 +80,79 @@ class Message {
 		$start_date = new DateTime($row['date']); //Time of post
 		$end_date = new DateTime($date_time_now); //Current time
 		$interval = $start_date->diff($end_date); //Difference between dates 
-		if($interval->y >= 1) {
+        if($interval->y >= 1) 
+        {
 			if($interval->y == 1)
 				$time_message = $interval->y . " year ago"; //1 year ago
 			else 
 				$time_message = $interval->y . " years ago"; //1+ year ago
 		}
-		else if ($interval->m >= 1) {
-			if($interval->d == 0) {
+        else if ($interval->m >= 1) 
+        {
+            if($interval->d == 0) 
+            {
 				$days = " ago";
 			}
-			else if($interval->d == 1) {
+            else if($interval->d == 1) 
+            {
 				$days = $interval->d . " day ago";
 			}
-			else {
+            else 
+            {
 				$days = $interval->d . " days ago";
 			}
 
 
-			if($interval->m == 1) {
+            if($interval->m == 1) 
+            {
 				$time_message = $interval->m . " month". $days;
 			}
-			else {
+            else 
+            {
 				$time_message = $interval->m . " months". $days;
 			}
 
 		}
-		else if($interval->d >= 1) {
-			if($interval->d == 1) {
+        else if($interval->d >= 1) 
+        {
+            if($interval->d == 1)
+            {
 				$time_message = "Yesterday";
 			}
-			else {
+            else 
+            {
 				$time_message = $interval->d . " days ago";
 			}
 		}
-		else if($interval->h >= 1) {
-			if($interval->h == 1) {
+        else if($interval->h >= 1)
+        {
+            if($interval->h == 1) 
+            {
 				$time_message = $interval->h . " hour ago";
 			}
-			else {
+            else 
+            {
 				$time_message = $interval->h . " hours ago";
 			}
 		}
-		else if($interval->i >= 1) {
-			if($interval->i == 1) {
+        else if($interval->i >= 1) 
+        {
+            if($interval->i == 1) 
+            {
 				$time_message = $interval->i . " minute ago";
 			}
-			else {
+            else 
+            {
 				$time_message = $interval->i . " minutes ago";
 			}
 		}
 		else {
-			if($interval->s < 30) {
+            if($interval->s < 30) 
+            {
 				$time_message = "Just now";
 			}
-			else {
+            else 
+            {
 				$time_message = $interval->s . " seconds ago";
 			}
 		}
@@ -135,22 +164,26 @@ class Message {
 		return $details_array;
 	}
 
-	public function getConvos() {
+    public function getConvos() 
+    {
 		$userLoggedIn = $this->user_obj->getUsername();
 		$return_string = "";
 		$convos = array();
 
 		$query = mysqli_query($this->con, "SELECT user_to, user_from FROM messages WHERE user_to='$userLoggedIn' OR user_from='$userLoggedIn' ORDER BY id DESC");
 
-		while($row = mysqli_fetch_array($query)) {
+        while($row = mysqli_fetch_array($query)) 
+        {
 			$user_to_push = ($row['user_to'] != $userLoggedIn) ? $row['user_to'] : $row['user_from'];
 
-			if(!in_array($user_to_push, $convos)) {
+            if(!in_array($user_to_push, $convos)) 
+            {
 				array_push($convos, $user_to_push);
 			}
 		}
 
-		foreach($convos as $username) {
+        foreach($convos as $username) 
+        {
 			$user_found_obj = new \One\User($this->con, $username);
 			$latest_message_details = $this->getLatestMessage($userLoggedIn, $username);
 
@@ -171,7 +204,8 @@ class Message {
 
 	}
 
-	public function getConvosDropdown($data, $limit) {
+    public function getConvosDropdown($data, $limit) 
+    {
 
 		$page = $data['page'];
 		$userLoggedIn = $this->user_obj->getUsername();
@@ -187,10 +221,12 @@ class Message {
 
 		$query = mysqli_query($this->con, "SELECT user_to, user_from FROM messages WHERE user_to='$userLoggedIn' OR user_from='$userLoggedIn' ORDER BY id DESC");
 
-		while($row = mysqli_fetch_array($query)) {
+        while($row = mysqli_fetch_array($query)) 
+        {
 			$user_to_push = ($row['user_to'] != $userLoggedIn) ? $row['user_to'] : $row['user_from'];
 
-			if(!in_array($user_to_push, $convos)) {
+            if(!in_array($user_to_push, $convos)) 
+            {
 				array_push($convos, $user_to_push);
 			}
 		}
@@ -198,7 +234,8 @@ class Message {
 		$num_iterations = 0; //Number of messages checked 
 		$count = 1; //Number of messages posted
 
-		foreach($convos as $username) {
+        foreach($convos as $username) 
+        {
 
 			if($num_iterations++ < $start)
 				continue;
@@ -241,7 +278,8 @@ class Message {
 		return $return_string;
 	}
 
-	public function getUnreadNumber() {
+    public function getUnreadNumber() 
+    {
 		$userLoggedIn = $this->user_obj->getUsername();
 		$query = mysqli_query($this->con, "SELECT * FROM messages WHERE viewed='no' AND user_to='$userLoggedIn'");
 		return mysqli_num_rows($query);
