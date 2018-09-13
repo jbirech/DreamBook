9aecf722e16ad865bab5d678c54c85d043cde333
Joe Birech <jobirech@e1z4r13p11.42.us.org>
2018-09-13 00:30:02 -0700

    some new files

---
+++ Joe_birech.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Joe Birech</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <!-- A picture of myself-->
+    <section class="image">
+        <img src="assets/images/Joe/Joe.png" alt="">
+    </section>
+
+    <!-- A little bit about myself -->
+    <section class="brief_history">
+
+    </section>
+
+    <!-- A little bit about my education -->
+    <section class="my_education">
+
+    </section>
+    
+    <!--Conclusions and my future ambitions -->
+    <section class="conclusion">
+
+    </section>
+</body>
+</html>
