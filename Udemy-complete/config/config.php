51611df071a6fcdfbd1644b613d512e69754f574
Joe Birech <jobirech@e1z4r13p11.42.us.org>
2018-10-10 21:57:40 -0700

    the complete source code for testing

---
+++ Udemy-complete/config/config.php
@@ -0,0 +1,14 @@
+<?php
+ob_start(); //Turns on output buffering 
+session_start();
+
+$timezone = date_default_timezone_set("America/los_Angeles");
+
+$con = mysqli_connect("localhost", "root", "", "dream-book"); //Connection variable
+
+if(mysqli_connect_errno()) 
+{
+	echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+?>
